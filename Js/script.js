62d4d650db1817f8f2123b495cba0e2da56d1e1e
MoringaSchool <winnieadhiambo600@gmail.com>
2018-09-18 14:14:40 +0300

    add change on html file

--- Js/script.js
+++ Js/script.js
@@ -1,26 +1,21 @@
 function triangleTester(){
 var ThreeSides = []
 
-ThreeSides.push(parseInt(prompt("enter first side")));
-ThreeSides.push(parseInt(prompt("enter second side")));
-ThreeSides.push(parseInt(prompt("enter third side")));
+ThreeSides.push(document.getElementById('1').value);
+ThreeSides.push(document.getElementById('2').value);
+ThreeSides.push(document.getElementById('3').value);
 
 var side1 = ThreeSides[0];
 var side2 = ThreeSides[1];
 var side3 = ThreeSides[2];
 
-if (side1===side2  &&  side2===side3 && side1===side3){
-result.innerHTML=(" It is an Equilateral Triangle")
-}
-
-else if (side1===side2  &&  side1!==side3 || side1===side3  &&  side1!==side2 || side2===side3  &&  side2!==side1){
-result.innerHTML=("It is an Isosceles Triangle")
-}
-
-else if(side1!==side2 && side2!==side3 && side1!==side3){
-result.innerHTML=("It is a Scalene Triangle")
-}
-else {
-alert("NOT a triangle")
+if (side1 + side2 <= side3 || side1 + side3 <= side2 ||side2 + side3 <= side1) {
+alert("not a triangle");
+} else if(side1==side2 && side2==side3 && side1==side3) {
+  alert("Equilateral");
+}else if(side1==side2 || side2==side3 || side1==side3) {
+  alert("Isosceles");
+}else if(side1!==side2 && side2!==side3) {
+  alert("scalene");
 }
 }
